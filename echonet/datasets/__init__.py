fcf9fc950d8c9cce0ff4369dca648f650c915b49
yourname <dingjieqiong@nefu.edu.cn>
2025-08-05 10:37:46 +0800

    1

--- echonet/datasets/__init__.py
+++ echonet/datasets/__init__.py
@@ -4,5 +4,7 @@ echocardiogram videos.
 """
 
 from .echo import Echo
+from .camus import HEART_datasets
+from .hmcqu import get_hmcqu_dataset
 
-__all__ = ["Echo","camus"]
+__all__ = ["Echo","HEART_datasets","get_hmcqu_dataset"]
